17d5e5e90d0827298b9a54f618205ff19676c1b1
Martin Butts <martinbutts@Martins-MacBook-Air.local>
2016-06-20 13:46:59 +1200

    second minesweeper save

--- minesweeper.js
+++ minesweeper.js
@@ -8,11 +8,17 @@ function startGame () {
 function addListeners (element) {
 	for (var i = 0; i < element.length; i++) {
 	element[i].addEventListener('click', showCell)
+	element[i].addEventListener('contextmenu', markCell)
 	}
 }
 
 function showCell (evt) {
 	evt.target.classList.remove('hidden')
+}
+
+function markCell (evt) {
+	evt.preventDefault()
+	evt.target.classList.toggle('marked')
 }
 
 startGame()
