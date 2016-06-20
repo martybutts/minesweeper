af64eaf79824dc79037399ed9caaac997c9acff3
Martin Butts <martinbutts@Martins-MacBook-Air.local>
2016-06-20 13:23:15 +1200

    first minesweeper save

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,18 @@
+
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+	addListeners(document.getElementsByClassName('board')[0].children)
 }
+
+function addListeners (element) {
+	for (var i = 0; i < element.length; i++) {
+	element[i].addEventListener('click', showCell)
+	}
+}
+
+function showCell (evt) {
+	evt.target.classList.remove('hidden')
+}
+
+startGame()
